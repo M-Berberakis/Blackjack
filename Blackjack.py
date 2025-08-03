76880200acab5db79283f0b7ed696b78c1d7f6a1
M-Berberakis <mihalis1999@gmail.com>
2025-08-03 14:21:56 +0300

    basic check system

--- Blackjack.py
+++ Blackjack.py
@@ -8,45 +8,32 @@ def pick_card():
 
 def player():
     total = 0
-    while True:
+    num = 3
+    while num == 3:
         p_input=input("Would you like to play? (Y)/(N): ")
 
         if p_input == 'Y':
             card, c_value = pick_card()
             print(f"{card} ({c_value})")
             total += c_value
-            
+
+            if check_bust(total):
+                print(f"{total} You've gone bust!")
+                break
 
         elif p_input == 'N':
-            print("Ok, cya!")
-            return total
+            print(f"Your total is: {total}")
+            total += c_value
+            break
         else:
             print("Please pick y or n")
 
-def check_bust():
-    #Uses total value to check whether the player has
-    #gone bust or not
+
+
+def check_bust(total):
+    return total >= 22
+    
             
 
 play = player()
-
-
-
-
-
-
-
-    
-    
-    
-    
-            
-
-
-
-
-
-
-
-
 
